59467721caecb675413aacb7c102c777b7a8a146
fer-123456 <66699055+fer-123456@users.noreply.github.com>
2020-06-20 13:06:36 -0500

    Add files via upload

---
+++ Layouts/header.php
@@ -0,0 +1,35 @@
+<nav class="navbar navbar-inverse">
+	<div class="container-fluid">
+		<div class="navbar-header">			
+			<a class="navbar-brand" href="?menu=home">
+			<span class="glyphicon" aria-hidden="true" ></span>
+			Proyecto Agro
+			</a>
+		</div>
+			<ul class="nav navbar-nav">
+				<li>
+					<a href="Componentes/inicio.php">Inicio</a>
+				</li>
+				<li>
+					<a href="?menu=regproducto">Registrar Productor</a>
+				</li> 
+				<li>
+					<a href="?menu=regcomprador">Registrar Comprador</a>
+				</li> 
+				
+				
+				<li>
+					<a href="?menu=regtransportista">Registrar Transportista</a>
+        		</li>   
+				<li>
+					<a href="?menu=ingreproduct">Ingresar Productos</a>
+				</li>
+				<li>
+					<a href="?menu=consulta">Consultar Productos</a>
+				</li>
+				<li>
+					<a href="?menu=produnegoc">Productos a Negociar</a>
+				</li>
+			</ul>
+	</div>
+</nav>
---
+++ routing.php
@@ -0,0 +1,91 @@
+<?php 
+	/**
+	* Archivo que redirecciona al contenido que se va incrustar dentro de la header y el footer
+	**/
+	/**switch ($_GET['menu']) {
+		case 'ingreso':
+			require_once('Componentes/ingreso.php');
+			break;
+		case 'ingreproduct':
+			require_once('Componentes/ingreproduct.php');
+			break;
+		case 'consulta':
+			require_once('Componentes/consultas.php');
+			break;
+		case 'produnegoc':
+			require_once('Componentes/produnegoc.php');
+			break;
+		case 'productor':
+			require_once('Componentes/productor.php');
+			break;
+		case 'transportista':
+			require_once('Componentes/transportista.php');
+			break;
+		case 'comprador':
+			require_once('Componentes/comprador.php');
+			break;
+		case 'registro':
+			require_once('Componentes/registro.php');
+			break;
+		case 'home':
+			require_once('Componentes/home.php');
+			break;
+		case 'regproducto':
+			require_once('Componentes/regproducto.php');
+			break;
+		case 'regtransportista':
+			require_once('Componentes/regtransportista.php');
+			break;
+		case 'regcomprador':
+			require_once('Componentes/regcomprador.php');
+			break;
+			case 'formulario_producto':
+				require_once('Componentes/formulario_producto.php');
+				break;
+			
+			case 'C_Login_transportista':
+				require_once('Componentes/C_Login_transportista.php');
+				break;
+		case 'P_Productor_saludo':
+				require_once('Componentes/P_Productor_saludo.php');
+				break;
+        case 'inicio':
+			require_once('Componentes/inicio.php');
+			break;	
+			case 'C_Comprador_saludo':
+				require_once('Componentes/C_Comprador_saludo.php');
+				break;	
+				case 'T_Transportador_saludo':
+					require_once('Componentes/T_Transportador_saludo.php');
+					break;	
+					
+					case 'C_Login_comprador':
+						require_once('Componentes/C_Login_comprador.php');
+						break;
+						case 'C_Login_productor':
+							require_once('Componentes/C_Login_productor.php');
+							break;		
+							
+							case 'C_Login_productor':
+								require_once('Componentes/C_Login_productor.php');
+								break;
+								case 'P_Registro_Transportista':
+									require_once('Componentes/P_Registro_Transportista.php');
+									break;		
+									
+									case 'P_Registro_Productor':
+										require_once('Componentes/P_Registro_Productor.php');
+										break;
+										
+										case 'P_Registro_Comprador':
+											require_once('Componentes/P_Registro_Comprador.php');
+											break;
+											case 'formulario2_producto':
+												require_once('Componentes/formulario2_producto.php');
+												break;
+												case 'Confirmacion_producto':
+													require_once('Componentes/Confirmacion_producto.php');
+													break;
+											
+	}
+ ?>
